8929fffe5a1ff6c0590be3e089f94648008a42e0
Erick Birbe <erickcion@gmail.com>
2016-04-29 18:37:18 +0000

    [REF] openacademy: Removing description from __openerp__.py

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -4,13 +4,6 @@
 
     'summary': """Manage trainings""",
 
-    'description': """
-        Open Academy module for managing trainings:
-            - training courses
-            - training sessions
-            - attendees registration
-    """,
-
     'author': "Erick Birbe",
     'website': "http://erickcion.wordpress.com",
 
