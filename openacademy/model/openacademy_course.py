18630573da122314d91c6b061dacb2770068fb2c
Erick Birbe <erickcion@gmail.com>
2016-05-03 17:33:22 +0000

    [REF] openacademy: Inherit copy method to manage duplicated course names

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -1,4 +1,4 @@
-from openerp import fields, models
+from openerp import api, fields, models
 
 '''
 Model class for Course
@@ -16,6 +16,20 @@ class Course(models.Model):
        string='Responsible', index=True)
 
    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+
+   @api.multi
+   def copy(self, default=None):
+      default = dict(default or {})
+
+      copied_count = self.search_count(
+         [('name', '=like', u"Copy of {}%".format(self.name))])
+      if not copied_count:
+         new_name = u"Copy of {}".format(self.name)
+      else:
+         new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+      default['name'] = new_name
+      return super(Course, self).copy(default)
 
    _sql_constraints = [
       ('name_description_check',
