41144f2c64edbfcef629a4242b51a6e2a7482212
Erick Birbe <erickcion@gmail.com>
2016-05-07 02:11:00 +0000

    [REF] openacademy: Moving wizard view to a new file

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -22,6 +22,7 @@
         'security/ir.model.access.csv',
         'view/openacademy_course_view.xml',
         'view/openacademy_session_view.xml',
+        'view/wizard.xml',
         'view/partner_view.xml',
         'workflow/session.xml',
     ],
